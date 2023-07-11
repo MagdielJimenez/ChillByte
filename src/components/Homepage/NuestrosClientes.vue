b5e0874fe880d5b147e2adeeced5eb0daeb847bc
magdiel <elimjt777@gmail.com>
2023-07-10 21:43:39 -0600

    Seccion de clientes actualizacion

--- src/components/Homepage/NuestrosClientes.vue
+++ src/components/Homepage/NuestrosClientes.vue
@@ -1,19 +1,19 @@
 <template>
     <div id="NuestrosClientes">
         <h1>Creamos con nuestros clientes</h1>
-        <b-container fluid >
-            <b-row>
-                <b-col>
-                    <img width="170px" src="../../assets/img/clientes/pinguinoRengo.png" alt="PinguinoCharlie">
+        <b-container>
+            <b-row class="mb-3">
+                <b-col class="d-flex align-content-end">
+                    <img class="align-self-end" width="170px" src="../../assets/img/clientes/logoCharlie.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col>
-                    <img width="170px" src="../../assets/img/clientes/imefLogo.png" alt="PinguinoCharlie">
+                <b-col class="d-flex align-content-end">
+                    <img class="align-self-end" width="170px" src="../../assets/img/clientes/imefLogo.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col>
-                    <img width="170px" src="../../assets/img/clientes/logocinves.png" alt="PinguinoCharlie">
+                <b-col class="d-flex align-content-end">
+                    <img class="align-self-end" width="170px" src="../../assets/img/clientes/logocinves.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col>
-                    <img width="170px" src="../../assets/img/clientes/ZAVERIA.png" alt="PinguinoCharlie">
+                <b-col class="d-flex align-content-end">
+                    <img class="align-self-end" width="170px" src="../../assets/img/clientes/ZAVERIA_logo.png" alt="PinguinoCharlie">
                 </b-col>
                
             </b-row>
