4c860d20cbe4bcb6d26c48291ba6dcf2b578f6f0
magdiel <elimjt777@gmail.com>
2023-07-14 16:41:32 -0600

    Nuestros clientes arreglo de estilos

--- src/components/Homepage/NuestrosClientes.vue
+++ src/components/Homepage/NuestrosClientes.vue
@@ -1,18 +1,18 @@
 <template>
     <div id="NuestrosClientes">
-        <h1>Creamos con nuestros clientes</h1>
+        <h1><span class="text-purple">Creamos</span> con nuestros clientes.</h1>
         <b-container>
             <b-row class="mb-3">
-                <b-col class="d-flex align-content-end">
+                <b-col class="d-flex align-content-end justify-content-center" >
                     <img class="align-self-end" width="170px" src="../../assets/img/clientes/logoCharlie.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col class="d-flex align-content-end">
+                <b-col class="d-flex align-content-end justify-content-center" >
                     <img class="align-self-end" width="170px" src="../../assets/img/clientes/imefLogo.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col class="d-flex align-content-end">
+                <b-col class="d-flex align-content-end justify-content-center">
                     <img class="align-self-end" width="170px" src="../../assets/img/clientes/logocinves.png" alt="PinguinoCharlie">
                 </b-col>
-                <b-col class="d-flex align-content-end">
+                <b-col class="d-flex align-content-end justify-content-center">
                     <img class="align-self-end" width="170px" src="../../assets/img/clientes/ZAVERIA_logo.png" alt="PinguinoCharlie">
                 </b-col>
                
