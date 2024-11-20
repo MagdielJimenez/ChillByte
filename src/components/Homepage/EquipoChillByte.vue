6d78953e7fbd5c1cf4f21255debd7ec9c7c5bc56
MagdielJimenez <elimjt777@gmail.com>
2024-11-20 16:33:24 -0600

    last update equipo chillbyte

--- src/components/Homepage/EquipoChillByte.vue
+++ src/components/Homepage/EquipoChillByte.vue
@@ -3,30 +3,19 @@
     <h1 class="pl-2 thin h1-size">El equipo que lo hará <span class="text-purple">posible</span>.</h1>
     <b-container fluid>
       <b-row>
-        <b-col sm="12" md="3" class="d-flex justify-content-center">
-          <div v-if="isVisible" class="bounce-in-down-enter-active">
-
-            <b-card title="Ricardo López Ortiz"
-              img-src="https://firebasestorage.googleapis.com/v0/b/bloona-55051.appspot.com/o/FotoRichi.png?alt=media&token=6211aa03-3914-4bed-a522-5dd750d9d7bd"
-              img-alt="Image" img-top tag="article" style="max-width: 20rem" class="mb-2">
-              <b-card-text>
-                <p>Desarrollador Netsuite, web y móvil</p>
-              </b-card-text>
-            </b-card>
-          </div>
-        </b-col>
-        <b-col sm="12" md="3" class="d-flex justify-content-center">
+        
+        <b-col sm="12" md="6" class="d-flex justify-content-center">
           <div v-if="secondIsVisible" class="bounce-in-down-enter-active">
             <b-card title="Magdiel Jiménez Tabla"
-              img-src="https://firebasestorage.googleapis.com/v0/b/bloona-55051.appspot.com/o/FotoMag.png?alt=media&token=8df97263-2f3e-4cba-be9c-d48b803fa9f2"
+              img-src="https://firebasestorage.googleapis.com/v0/b/bloona-55051.appspot.com/o/Foto%20Mag%20Chillbyte2.png?alt=media&token=ebaf6791-89db-4e3a-addd-ad05f94e4361"
               img-alt="Image" img-top tag="article" style="max-width: 20rem;margin:0px;" class="mb-2">
               <b-card-text>
-                <p>Desarrolladora web y móvil</p>
+                <p>Desarrolladora Netsuite, Web y Móvil</p>
               </b-card-text>
             </b-card>
           </div>
         </b-col>
-        <b-col sm="12" md="3" class="d-flex justify-content-center">
+        <b-col sm="12" md="6" class="d-flex justify-content-center">
           <div v-if="thirdIsVisible" class="bounce-in-down-enter-active">
             <b-card title="Francisco Vaca Silva"
               img-src="https://firebasestorage.googleapis.com/v0/b/bloona-55051.appspot.com/o/FotoPacoVaca.png?alt=media&token=7842bfe7-75fa-4e72-984d-7e6d9bde1f90"
@@ -38,19 +27,7 @@
           </div>
 
         </b-col>
-        <b-col sm="12" md="3" class="d-flex justify-content-center">
-          <div v-if="fourthIsVisible" class="bounce-in-down-enter-active">
-
-            <b-card title="Adrián Aguilar"
-              img-src="https://firebasestorage.googleapis.com/v0/b/bloona-55051.appspot.com/o/FotoPaco.png?alt=media&token=72d68281-3872-4927-a6eb-c553b9da3395"
-              img-alt="Image" img-top tag="article" style="max-width: 20rem" class="mb-2">
-              <b-card-text>
-                <p>Arquitecto de Soluciones</p>
-              </b-card-text>
-            </b-card>
-          </div>
-
-        </b-col>
+        
       </b-row>
     </b-container>
   </div>
